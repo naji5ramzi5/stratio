751daf4870a8111cf26451eb96b3b4f13f300522
stratocrypto <stratocrypto1@gmail.com>
2024-12-10 21:16:45 +0300

    Update train.py

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ import csv
 import os
 import pytz
 from datetime import datetime, timedelta
-logger = logging.getLogger(_name_)
+logger = logging.getLogger(__name__)
 import pandas as pd
 
 
@@ -395,7 +395,7 @@ def main(cfg: DictConfig) -> None:
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, partial(handle_prediction, cfg=cfg)))  # تمرير cfg هنا
     application.run_polling()
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     # flask_thread = Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 8080})
     # flask_thread.start()
     main()
