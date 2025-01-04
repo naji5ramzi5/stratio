e9c5af8a9dad6f675ab1c4071f95fc2e4a22a8d9
stratocrypto <stratocrypto1@gmail.com>
2025-01-04 14:17:12 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import logging
 import os
-
 import hydra
 from omegaconf import DictConfig
 from models import MODELS
@@ -9,49 +8,33 @@ from factory.trainer import Trainer
 from factory.evaluator import Evaluator
 from factory.profit_calculator import ProfitCalculator
 import pandas as pd
-
 from sklearn.model_selection import TimeSeriesSplit
 from path_definition import HYDRA_PATH
-
 from utils.reporter import Reporter
 from data_loader.creator import create_dataset, preprocess
-
-
-
 import yaml
 from datetime import datetime, timedelta
-
 import requests
 import csv
-import os
 import pytz
 from datetime import datetime, timedelta
-logger = logging.getLogger(__name__)
-import pandas as pd
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from flask import Flask
+from threading import Thread
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
 
+logger = logging.getLogger(__name__)
 
 # إعداد تفاصيل API
 url = "https://api.binance.com/api/v3/klines"
-# "ALGOUSDT", "MANAUSDT", "ENJUSDT","YFIUSDT", "EGLDUSDT", "ONEUSDT", "ANKRUSDT", "HOTUSDT", "SCUSDT","OXTUSDT", "STORJUSDT","BADGERUSDT", "LTOUSDT"
-symbols  = [];
+symbols = []
 
-
-# current_date = datetime.now()
-
-# data = {
-#     "window_size": 5,
-#     "train_start_date": "2020-01-01 13:30:00",  # تاريخ ثابت
-#     "train_end_date": (current_date - timedelta(days=5)).strftime("%Y-%m-%d 09:30:00"),
-#     "valid_start_date": (current_date - timedelta(days=5)).strftime("%Y-%m-%d 10:30:00"),
-#     "valid_end_date": (current_date + timedelta(days=2)).strftime("%Y-%m-%d 10:30:00"),
-#     "features": "Date, open, High, Low, close, volume",
-#     "indicators_names": "rsi macd"
-# }
-
-# # كتابة البيانات إلى ملف YAML بدون علامات التنصيص
-# with open("configs/hydra/dataset_loader/common.yaml", "w") as file:
-#     yaml.dump(data, file, default_flow_style=False, allow_unicode=True, sort_keys=False)
-
+data_folder = '/opt/render/project/src/data'
+if not os.path.exists(data_folder):
+    os.makedirs(data_folder)
+data_filename = os.path.join(data_folder, 'data1.csv')
 
 
 def check_and_delete_file(filename):
@@ -64,7 +47,7 @@ def check_and_delete_file(filename):
                 return None, False
             
             # التحقق من وجود رأس (Header) وتجاهله
-            if lines[0].strip().startswith('timestamp'):  # إذا كان السطر الأول رأس
+            if lines[0].strip().startswith('timestamp'):
                 print(f"Header detected in file {filename}, skipping the first line.")
                 lines = lines[1:]
 
@@ -113,6 +96,7 @@ def check_and_delete_file(filename):
         print(f"An error occurred while checking file {filename}: {e}")
         return None, False
 
+
 def add_future_dates(filename, symbol):
     today = datetime.now(pytz.utc).replace(hour=0, minute=0, second=0, microsecond=0)
     future_dates = [today + timedelta(days=i) for i in range(3)]  # today and the next two days
@@ -121,26 +105,11 @@ def add_future_dates(filename, symbol):
         writer = csv.writer(file)
         for future_date in future_dates:
             formatted_date = future_date.strftime('%Y-%m-%d 00:00:00+00:00')
-            writer.writerow([formatted_date, symbol,'1','1','1','1','1'])
+            writer.writerow([formatted_date, symbol, '1', '1', '1', '1', '1'])
 
-
-data_folder = '/opt/render/project/src/data'
-
-if not os.path.exists(data_folder):
-    os.makedirs(data_folder)
-
-data_filename = os.path.join(data_folder,'data1.csv')
 
 def fetch_and_save_data(symbol, start_date, end_date):
-    url = "https://api.binance.com/api/v3/klines"  # Add API URL here
-    # params = {
-    #     'symbol': symbol,
-    #     'interval': '1d',
-    #     'startTime': int(start_date.timestamp() * 1000),
-    #     'endTime': int(end_date.timestamp() * 1000)
-    # }
-
-    # //
+    url = "https://api.binance.com/api/v3/klines"
     API_KEY = 'ddCXARf1hp1OjbaLJInHpYnEhMqKziYs9ae8dEH1NbLaonYpkgPu0tX75DqnjaDD'
     API_SECRET = 'oFHovFudTJcj9UteGQa3VxxIOp9OqvlPn7t9HWiHJ62afPvgvZVo7Id01VsVRHW2'
 
@@ -152,17 +121,13 @@ def fetch_and_save_data(symbol, start_date, end_date):
         "limit": 1000
     }
     headers = {"X-MBX-APIKEY": API_KEY}
-    
-    response = requests.get(url, params=params, headers=headers)
 
-    # //
-    
-    response = requests.get(url, params=params)
+    response = requests.get(url, params=params, headers=headers)
     data = response.json()
 
-    if isinstance(data, dict) and 'code' in data and data['code'] == -1121: 
+    if isinstance(data, dict) and 'code' in data and data['code'] == -1121:
         return False
-    
+
     # Check if data is available
     if not data:
         print(f"No data available for {symbol} from {start_date.date()}")
@@ -173,11 +138,11 @@ def fetch_and_save_data(symbol, start_date, end_date):
         writer = csv.writer(file)
         for entry in data:
             timestamp = datetime.fromtimestamp(entry[0] / 1000, tz=pytz.utc).strftime('%Y-%m-%d 00:00:00+00:00')
-            writer.writerow([
-                timestamp, symbol, entry[1], entry[2], entry[3], entry[4], entry[5]
-            ])
-    
-    return True  # Data fetched successfully
+            writer.writerow([timestamp, symbol, entry[1], entry[2], entry[3], entry[4], entry[5]])
+
+    return True  # Data fetched successfully
+
+
 def get_usd_and_usdt_pairs():
     url = "https://api.binance.com/api/v3/exchangeInfo"
     try:
@@ -194,177 +159,10 @@ def get_usd_and_usdt_pairs():
         return []
 
 
-def train(cfg: DictConfig): 
-    # جلب الأزواج النشطة التي تحتوي على USDT أو USD
-    symbols = get_usd_and_usdt_pairs()
-
-    # طباعة اللائحة
-    print("List of Active Trading Pairs (USDT/USD) on Binance:")
-    print(len(symbols))
-    start_date = datetime(2022, 1, 1, tzinfo=pytz.utc)
-    end_date = datetime.now(pytz.utc) - timedelta(days=1)
-    period = timedelta(days=90)
-
-    title = ''
-    increase_threshold = 0.03
-    saved_percentage = 0
-  
-    for symbol in symbols:
-        try:
-            # Check if file exists and delete it if necessary
-            if os.path.exists(data_filename):
-                os.remove(data_filename)  # Delete the file if it exists
-
-            # Write headers only when creating the file
-            with open(data_filename, 'a', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow(['timestamp', 'symbol', 'open', 'high', 'low', 'close', 'volume'])
-
-            current_start = start_date
-            data_available = False
-
-            while current_start < end_date:
-                current_end = min(current_start + period, end_date)
-                print(f"Fetching data for {symbol} from {current_start.date()} to {current_end.date()}")
-                
-                # Fetch data for each period
-                if fetch_and_save_data(symbol, current_start, current_end):
-                    data_available = True  # Data fetched successfully for at least one period
-                if not data_available:
-                    break
-                current_start = current_end + timedelta(days=1)
-            
-            if not data_available:
-                print(f"Skipping {symbol} due to insufficient data.")
-                continue  # Skip symbol if no data is available
-
-            yesterday_close, data_complete = check_and_delete_file(data_filename)
-            if not data_complete:
-                continue
-            # Add three future dates at the end of the file with the symbol
-            add_future_dates(data_filename, symbol)
-
-            print("Data download complete for all symbols with data from the beginning of 2020.")
-            
-            # Load dataset or model based on configuration
-            if cfg.load_path is None and cfg.model is None:
-                msg = 'either specify a load_path or config a model.'
-                logger.error(msg)
-                raise Exception(msg)
-
-            elif cfg.load_path is not None:
-                dataset_ = pd.read_csv(cfg.load_path)
-                if 'Date' not in dataset_.keys():
-                    dataset_.rename(columns={'timestamp': 'Date'}, inplace=True)
-                if 'High' not in dataset_.keys():
-                    dataset_.rename(columns={'high': 'High'}, inplace=True)
-                if 'Low' not in dataset_.keys():
-                    dataset_.rename(columns={'low': 'Low'}, inplace=True)
-
-                dataset, profit_calculator = preprocess(dataset_, cfg, logger)
-
-            elif cfg.model is not None:
-                dataset, profit_calculator = get_dataset(cfg.dataset_loader.name, "2022-01-01 13:30:00",
-                                                        (datetime.now() + timedelta(days=2)).strftime("%Y-%m-%d 10:30:00"), cfg)
-
-            cfg.save_dir = os.getcwd()
-            reporter = Reporter(cfg)
-            reporter.setup_saving_dirs(cfg.save_dir)
-            model = MODELS[cfg.model.type](cfg.model)
-
-            dataset_for_profit = dataset.copy()
-            dataset_for_profit.drop(['prediction'], axis=1, inplace=True)
-            dataset.drop(['predicted_high', 'predicted_low'], axis=1, inplace=True)
-         
-            if cfg.validation_method == 'simple':
-                train_dataset = dataset[ (dataset['Date'] > "2022-01-01 13:30:00") & (dataset['Date'] < (datetime.now() - timedelta(days=5)).strftime("%Y-%m-%d 09:30:00"))]
-                valid_dataset = dataset[ (dataset['Date'] > (datetime.now() - timedelta(days=5)).strftime("%Y-%m-%d 10:30:00")) & (dataset['Date'] < (datetime.now() + timedelta(days=2)).strftime("%Y-%m-%d 10:30:00"))]
-                Trainer(cfg, train_dataset, None, model).train()
-                mean_prediction = Evaluator(cfg, test_dataset=valid_dataset, model=model, reporter=reporter).evaluate()
-              
-            elif cfg.validation_method == 'cross_validation':
-                n_split = 3
-                tscv = TimeSeriesSplit(n_splits=n_split)
-
-                for train_index, test_index in tscv.split(dataset):
-                    train_dataset, valid_dataset = dataset.iloc[train_index], dataset.iloc[test_index]
-                    Trainer(cfg, train_dataset, None, model).train()
-                    mean_prediction = Evaluator(cfg, test_dataset=valid_dataset, model=model, reporter=reporter).evaluate()
-
-                reporter.add_average()
-            
-            # Calculate profit and prediction data
-            x = ProfitCalculator(cfg, dataset_for_profit, profit_calculator, mean_prediction, reporter).profit_calculator()
-            predicted_high = x[0]['predicted_high'].iloc[0]
-            predicted_low = x[0]['predicted_low'].iloc[0]
-            predicted_mean = x[0]['predicted_mean'].iloc[0]
-            predicted_high_formated = "{:.18f}".format(predicted_high)
-            predicted_low_formated = "{:.18f}".format(predicted_low)
-            predicted_mean_formated = "{:.18f}".format(predicted_mean)
-            increase = (predicted_mean - yesterday_close) / yesterday_close
-            if increase > increase_threshold:
-                saved_percentage = increase * 100
-            else:
-                continue  # Skip this iteration if the increase is below the threshold
-            
-            predicted_low_finally = 0
-            predicted_high_finally = 0
-            if predicted_low_formated > predicted_high_formated:
-                predicted_low_finally = predicted_high_formated
-                predicted_high_finally = predicted_low_formated
-            else:
-                predicted_low_finally = predicted_low_formated
-                predicted_high_finally = predicted_high_formated
-
-            title += f'رمز العملة: {symbol}\n'
-            title += f'نسبة الزيادة المتوقعة: {round(saved_percentage, 1)}%\n'
-            title += f'اعلى سعر متوقع لليوم⬆️:\n {predicted_high_finally}\n'
-            title += f'اقل سعر متوقع لليوم⬇️:\n {predicted_low_finally}\n'
-            title += f'سعر الإغلاق المتوقع لليوم:\n {predicted_mean_formated}\n'
-            title += '---\n'
-            print('..............................d')
-            print(yesterday_close)
-            reporter.print_pretty_metrics(logger)
-            reporter.save_metrics()
-
-        except Exception as e:
-            print(f"Error occurred while processing symbol {symbol}: {str(e)}")
-            continue  # Continue to the next symbol if an error occurs
-
-    # title += 'لا تجعل التنبؤات محور تداولك. ركز على التحليل العميق وإدارة المخاطر، واستند إلى البيانات والحقائق لاتخاذ قرارات مستنيرة.\n'
-    print(title)
-
-    return title  # Return the title or any other relevant data
-
-from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-import logging
-import hydra
-from omegaconf import DictConfig
-from functools import partial
-from flask import Flask
-from threading import Thread
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
-
-app = Flask(__name__)
-
-logging.basicConfig(level=logging.INFO)
-
-TOKEN = '7272871832:AAGa5-_FdFfziJqDG9pp4N9ljnZ2uyxslJ0'
-
-AUTHORIZED_USERS = [895650332,991558864,715531930,117245128,1796556765]#,1796556765,715531930,117245128
-
 async def data(update: Update, context: ContextTypes.DEFAULT_TYPE, cfg: DictConfig) -> None:
-    # الحصول على النتيجة الكبيرة
     result = train(cfg)
-
-    # تقسيم النص عند الفاصل ---
     parts = result.split('---')
-
-    # إرسال كل جزء من الأجزاء بشكل منفصل
     for part in parts:
-        # التأكد من أن الجزء ليس فارغًا قبل إرساله
         if part.strip():  # تأكد من أن الجزء ليس فقط مسافات فارغة
             await update.message.reply_text(part.strip())  # إرفاق أي مسافات غير ضرورية
 
@@ -375,53 +173,52 @@ async def check_authorized_user(update: Update) -> bool:
         return True
     return False
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if not await check_authorized_user(update):
         await update.message.reply_text('ليس لديك صلاحية للوصول إلى هذا البوت.')
         return
-    
+
     keyboard = [[KeyboardButton("توقع")]]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=False)
     await update.message.reply_text('مرحبًا! اضغط على الزر لتوقع النتيجة.', reply_markup=reply_markup)
+
 
 async def handle_prediction(update: Update, context: ContextTypes.DEFAULT_TYPE, cfg: DictConfig) -> None:
     if not await check_authorized_user(update):
         await update.message.reply_text('ليس لديك صلاحية للوصول إلى هذا البوت.')
         return
-    
+
     if update.message.text == "توقع":
         await data(update, context, cfg)
+
 
 async def daily_prediction(cfg: DictConfig, application: Application) -> None:
     for user_id in AUTHORIZED_USERS:
         try:
-            # الحصول على النتيجة من دالة train
             result = train(cfg)
-
-            # تقسيم الرسالة بناءً على الفاصل ---
             parts = result.split('---')
 
-            # إرسال كل جزء من الأجزاء بشكل منفصل
             for part in parts:
-                # التأكد من أن الجزء ليس فارغًا قبل إرساله
-                if part.strip():  # التأكد من عدم إرسال جزء فارغ
+                if part.strip():
                     await application.bot.send_message(user_id, part.strip())
-
         except Exception as e:
             print(f"فشل في إرسال التوقع إلى {user_id}: {e}")
-
 
 
 @hydra.main(config_path=HYDRA_PATH, config_name="train")
 def main(cfg: DictConfig) -> None:
     application = Application.builder().token(TOKEN).build()
-    # scheduler = AsyncIOScheduler()
-    # trigger = CronTrigger(hour=13, minute=41, second=30, timezone="Asia/Baghdad")
-    # scheduler.add_job(daily_prediction, trigger, args=[cfg, application])
-    # scheduler.start()
+
+    scheduler = AsyncIOScheduler()
+    trigger = CronTrigger(hour=6, minute=0, second=0, timezone="Asia/Baghdad")
+    scheduler.add_job(daily_prediction, trigger, args=[cfg, application])
+    scheduler.start()
+
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, partial(handle_prediction, cfg=cfg)))  # تمرير cfg هنا
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, partial(handle_prediction, cfg=cfg)))
     application.run_polling()
+
 
 if __name__ == '__main__':
     flask_thread = Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 8080})
