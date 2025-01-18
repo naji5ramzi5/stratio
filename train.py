ba9eef149e6f0dac7338e855fa475b2e3da94680
stratocrypto <stratocrypto1@gmail.com>
2025-01-18 13:28:07 +0300

    Update train.py

--- train.py
+++ train.py
@@ -503,7 +503,7 @@ def main(cfg: DictConfig) -> None:
     
     # إعداد الجدولة اليومية
     scheduler = AsyncIOScheduler()
-    trigger = CronTrigger(hour=15, minute=0, second=0, timezone="Asia/Baghdad")
+    trigger = CronTrigger(hour=14, minute=0, second=0, timezone="Asia/Baghdad")
     scheduler.add_job(daily_prediction, trigger, args=[cfg, application])
     
     # تشغيل الجدولة بشكل متزامن
