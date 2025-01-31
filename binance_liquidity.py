32fbd3d56e5a49ed3abecc8d2691e0b0bdea7f9e
stratocrypto <stratocrypto1@gmail.com>
2025-01-31 03:00:30 +0300

    Create binance_liquidity.py

---
+++ binance_liquidity.py
@@ -0,0 +1,22 @@
+from binance.client import Client
+
+# إدخال API Key الخاص بك من Binance
+API_KEY = "ddCXARf1hp1OjbaLJInHpYnEhMqKziYs9ae8dEH1NbLaonYpkgPu0tX75DqnjaDD"
+API_SECRET = "oFHovFudTJcj9UteGQa3VxxIOp9OqvlPn7t9HWiHJ62afPvgvZVo7Id01VsVRHW2"
+
+# تهيئة الاتصال بواجهة Binance API
+client = Client(API_KEY, API_SECRET)
+
+def check_liquidity_and_price(symbol: str) -> bool:  
+    message=''
+    klines = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_15MINUTE, limit=10)
+    
+    old_volume = float(klines[0][7])
+    new_volume = float(klines[8][7])
+
+    if old_volume > 0 and new_volume > 0:
+        drop_percentage = (old_volume - new_volume) / old_volume
+    
+    message +=f'{drop_percentage * 100:.2f}'
+
+    return message
