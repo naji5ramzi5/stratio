9ca7914410a18bc91241f5d682f59b580d1bc09d
stratocrypto <stratocrypto1@gmail.com>
2025-02-01 22:02:00 +0300

    Update crypto_sentiment_analyzer.py

--- crypto_sentiment_analyzer.py
+++ crypto_sentiment_analyzer.py
@@ -3,7 +3,7 @@ from textblob import TextBlob
 
 class CryptoSentimentAnalyzer:
     NEWS_API_URL = "https://newsapi.org/v2/everything"
-    API_KEY = "00b61f31bfce4f4282e44161fcadea48"  # استبدل بمفتاح NewsAPI الخاص بك
+    API_KEY = "5c09530ca24f4251940216dc2d9be275"  # استبدل بمفتاح NewsAPI الخاص بك
 
     def __init__(self):
         pass
