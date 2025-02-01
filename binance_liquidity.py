c477d0756d48efbc25fadbb603d2da0b1aca178b
stratocrypto <stratocrypto1@gmail.com>
2025-02-01 12:48:54 +0300

    Update binance_liquidity.py

--- binance_liquidity.py
+++ binance_liquidity.py
@@ -8,15 +8,26 @@ API_SECRET = "oFHovFudTJcj9UteGQa3VxxIOp9OqvlPn7t9HWiHJ62afPvgvZVo7Id01VsVRHW2"
 client = Client(API_KEY, API_SECRET)
 
 def check_liquidity_and_price(symbol: str) -> bool:  
+    liquidity=''
     message=''
-    klines = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_15MINUTE, limit=10)
+
+    klines = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_15MINUTE, limit=8)
     
     old_volume = float(klines[0][7])
-    new_volume = float(klines[8][7])
+    new_volume = float(klines[6][7])
 
     if old_volume > 0 and new_volume > 0:
         drop_percentage = (old_volume - new_volume) / old_volume
     
-    message +=f'{drop_percentage * 100:.2f}'
+    liquidity +=f'{drop_percentage * 100:.2f}'
+    print(float(liquidity))
+    if float(liquidity) <= 0:
+        message='لايوجد سحب للسيولة'
+
+    if float(liquidity) > 0 and float(liquidity) <= 20 :
+        message='سحب السيولة بسيط'
+
+    if float(liquidity) > 20 :
+        message='سحب السيولة كبير'
 
     return message
